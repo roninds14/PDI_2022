e0bf70ba531e4b41e5265f506d8c721ad129e787
Roni dos Santos <roninds14@gmail.com>
2022-06-09 23:02:44 -0300

    Correção formação da cor - Adição Histograma, Soma, Média, Mediana, Laplaciano e Realce Laplaciano

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ let buttonCinza = document.getElementById("btn-tons-cinza")
 let buttonSave = document.getElementById("btn-save")
 let buttonSalPimenta = document.getElementById("btn-tons-sal-pimenta")
 let buttonSepararTons = document.getElementById("btn-separar-tons")
+let buttonHistograma = document.getElementById("btn-histograma")
+let buttonSoma = document.getElementById("btn-soma")
+let buttonMedia = document.getElementById("btn-media")
+let buttonMediana = document.getElementById("btn-mediana")
+let buttonLaplaciano = document.getElementById("btn-laplaciano")
+let buttonRealceLaplaciano = document.getElementById("btn-realce-laplaciano")
 let inputFile = document.getElementById("input-file")
 let canvasOriginal = document.getElementById("img_00")
 let canvasProcessado = document.getElementById("img_01")
 let inImg
+const SOMA = 10 
 
 buttonImg.addEventListener("click", () => {
     inputFile.click()
@@ -29,11 +36,37 @@ buttonSave.addEventListener("click", () => {
 })
 
 buttonSalPimenta.addEventListener("click", () => {
-    ruidoSalPimenta()
+    const canvas = ruidoSalPimenta()
+    let ctx = canvasProcessado.getContext('2d')
+    ctx.drawImage(canvas,0,0)
 })
 
 buttonSepararTons.addEventListener("click",  () =>{
     separarTons()
+})
+
+buttonHistograma.addEventListener("click", () =>{
+    equalizacaoHistograma()
+})
+
+buttonSoma.addEventListener("click", () =>{
+    soma()
+})
+
+buttonMedia.addEventListener("click", () =>{
+    media()
+})
+
+buttonMediana.addEventListener("click", () =>{
+    mediana()
+})
+
+buttonLaplaciano.addEventListener("click", () =>{
+    laplaciano(8,8)
+})
+
+buttonRealceLaplaciano.addEventListener("click", () =>{
+    laplaciano(9,2)
 })
 
 window.addEventListener('DOMContentLoaded', () =>{
@@ -124,9 +157,10 @@ function ruidoSalPimenta(){
     
     const src = new Uint32Array(inImg.data.buffer)
 
-    const ctx = canvasProcessado.getContext('2d')
-    canvasProcessado.width = width
-    canvasProcessado.height = height
+    const canvas = document.createElement("canvas")
+    const ctx = canvas.getContext('2d')
+    canvas.width = width
+    canvas.height = height
 
     let row = 0
     let colunm = 0 
@@ -157,6 +191,8 @@ function ruidoSalPimenta(){
         ctx.fillRect(colunm,row,1,1);
         colunm++
     }
+
+    return canvas
 }
 
 function separarTons(){
@@ -208,9 +244,220 @@ function separarTons(){
 }
 
 function rgbToHex(r,g,b){
-    const red = r.toString(16)
-    const green = g.toString(16)
-    const blue = b.toString(16)
+    const red = r.toString(16).length == 1? "0" + r.toString(16): r.toString(16)
+    const green = g.toString(16).length == 1? "0" + g.toString(16): g.toString(16)
+    const blue = b.toString(16).length == 1? "0" + b.toString(16): b.toString(16)
 
     return "#" + red + green + blue
+}
+
+function equalizacaoHistograma() {
+    const src = new Uint32Array(inImg.data.buffer);
+    
+    let histBrightness = (new Array(256)).fill(0);
+    let histR = (new Array(256)).fill(0);
+    let histG = (new Array(256)).fill(0);
+    let histB = (new Array(256)).fill(0);
+    for (let i = 0; i < src.length; i++) {
+        let r = src[i] & 0xFF;
+        let g = (src[i] >> 8) & 0xFF;
+        let b = (src[i] >> 16) & 0xFF;
+        histBrightness[r]++;
+        histBrightness[g]++;
+        histBrightness[b]++;
+        histR[r]++;
+        histG[g]++;
+        histB[b]++;
+    }
+    
+    let maxBrightness = 0;
+    
+    for (let i = 1; i < 256; i++) {
+        if (maxBrightness < histBrightness[i]) {
+            maxBrightness = histBrightness[i]
+        }
+    }
+    
+    const canvas = document.getElementById('img_01');
+    const ctx = canvas.getContext('2d');
+    let guideHeight = 8;
+    let startY = (canvas.height - guideHeight);
+    let dx = canvas.width / 256;
+    let dy = startY / maxBrightness;
+    ctx.lineWidth = dx;
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    for (let i = 0; i < 256; i++) {
+        let x = i * dx;
+      
+        ctx.strokeStyle = "#000000";
+        ctx.beginPath();
+        ctx.moveTo(x, startY);
+        ctx.lineTo(x, startY - histBrightness[i] * dy);
+        ctx.closePath();
+        ctx.stroke(); 
+      
+        ctx.strokeStyle = 'rgb(' + i + ', ' + i + ', ' + i + ')';
+        ctx.beginPath();
+        ctx.moveTo(x, startY);
+        ctx.lineTo(x, canvas.height);
+        ctx.closePath();
+        ctx.stroke(); 
+    }
+}
+
+function soma(){
+    const size = inImg.width * inImg.height
+
+    let red = (new Array(size)).fill(0)
+    let green = (new Array(size)).fill(0)
+    let blue = (new Array(size)).fill(0)
+
+    for(let j = 0; j < SOMA; j++){
+        let canvas = ruidoSalPimenta()
+        let context = canvas.getContext('2d')
+
+        let somaImg = context.getImageData(0, 0, inImg.width, inImg.height)
+        
+        const src = new Uint32Array(somaImg.data.buffer)
+
+        for (let i = 0; i < src.length; i++) {
+            red[i] += src[i] & 0xFF
+            green[i] += (src[i] >> 8) & 0xFF
+            blue[i] += (src[i] >> 16) & 0xFF
+        }
+    }
+
+    const ctx = canvasProcessado.getContext('2d')
+    canvasProcessado.width = inImg.width
+    canvasProcessado.height = inImg.height
+
+    let row = 0
+    let colunm = 0 
+
+    for (let i = 0; i < size; i++) {
+        let r = parseInt(red[i]/SOMA)
+        let g = parseInt(green[i]/SOMA)
+        let b = parseInt(blue[i]/SOMA)
+
+        if(i && !(i % inImg.width)){
+            row++
+            colunm = 0
+        }
+
+        ctx.fillStyle = rgbToHex(r,g,b);
+        ctx.fillRect(colunm,row,1,1);
+        colunm++
+    }
+}
+
+function media(){
+    const {width, height} = inImg
+    const size = width*height
+    const src = new Uint32Array(inImg.data.buffer)
+
+    const ctx = canvasProcessado.getContext('2d')
+    canvasProcessado.width = inImg.width
+    canvasProcessado.height = inImg.height
+
+    let row = 0
+    let colunm = 0 
+    
+    for (let i = 0; i < size; i++) {
+        let r,g,b
+        if(!colunm || colunm == width-1 || !row || row == height-1){
+            r = src[i] & 0xFF
+            g = (src[i] >> 8) & 0xFF
+            b = (src[i] >> 16) & 0xFF
+        }else{
+            r = parseInt((( src[i-width-1] & 0xFF)+        (src[i-width] & 0xFF)+        (src[i-width+1] & 0xFF)+        (src[i-1] & 0xFF)+        (src[i] & 0xFF)+        (src[i+1] & 0xFF)+        (src[i+width-1] & 0xFF)+        (src[i+width] & 0xFF)+        (src[i+width+1] & 0xFF))/9)
+            g = parseInt((((src[i-width-1] >> 8) & 0xFF)+( (src[i-width] >> 8) & 0xFF)+ ((src[i-width+1] >> 8) & 0xFF)+ ((src[i-1] >> 8) & 0xFF)+ ((src[i] >> 8) & 0xFF)+ ((src[i+1] >> 8) & 0xFF)+ ((src[i+width-1] >> 8) & 0xFF)+ ((src[i+width] >> 8) & 0xFF)+ ((src[i+width+1] >> 8) & 0xFF))/9)
+            b = parseInt((((src[i-width-1] >> 16) & 0xFF)+((src[i-width] >> 16) & 0xFF)+((src[i-width+1] >> 16) & 0xFF)+((src[i-1] >> 16) & 0xFF)+((src[i] >> 16) & 0xFF)+((src[i+1] >> 16) & 0xFF)+((src[i+width-1] >> 16) & 0xFF)+((src[i+width] >> 16) & 0xFF)+((src[i+width+1] >> 16) & 0xFF))/9)
+        }
+
+        if(i && !(i % width)){
+            row++
+            colunm = 0
+        }
+
+        ctx.fillStyle = rgbToHex(r,g,b);
+        ctx.fillRect(colunm,row,1,1);
+        colunm++
+    }
+}
+
+function mediana(){
+    const {width, height} = inImg
+    const size = width*height
+    const src = new Uint32Array(inImg.data.buffer)
+
+    const ctx = canvasProcessado.getContext('2d')
+    canvasProcessado.width = inImg.width
+    canvasProcessado.height = inImg.height
+
+    let row = 0
+    let colunm = 0 
+    
+    for (let i = 0; i < size; i++) {
+        let r,g,b
+        if(!colunm || colunm == width-1 || !row || row == height-1){
+            r = src[i] & 0xFF
+            g = (src[i] >> 8) & 0xFF
+            b = (src[i] >> 16) & 0xFF
+        }else{
+            r = calculaMediana([( src[i-width-1] & 0xFF),(src[i-width] & 0xFF),(src[i-width+1] & 0xFF),(src[i-1] & 0xFF),(src[i] & 0xFF),(src[i+1] & 0xFF),(src[i+width-1] & 0xFF),(src[i+width] & 0xFF),(src[i+width+1] & 0xFF)])
+            g = calculaMediana([((src[i-width-1] >> 8) & 0xFF),((src[i-width] >> 8) & 0xFF),((src[i-width+1] >> 8) & 0xFF),((src[i-1] >> 8) & 0xFF),((src[i] >> 8) & 0xFF),((src[i+1] >> 8) & 0xFF),((src[i+width-1] >> 8) & 0xFF),((src[i+width] >> 8) & 0xFF), ((src[i+width+1] >> 8) & 0xFF)])
+            b = calculaMediana([((src[i-width-1] >> 16) & 0xFF),((src[i-width] >> 16) & 0xFF),((src[i-width+1] >> 16) & 0xFF),((src[i-1] >> 16) & 0xFF),((src[i] >> 16) & 0xFF),((src[i+1] >> 16) & 0xFF),((src[i+width-1] >> 16) & 0xFF),((src[i+width] >> 16) & 0xFF),((src[i+width+1] >> 16) & 0xFF)])
+        }
+
+        if(i && !(i % width)){
+            row++
+            colunm = 0
+        }
+
+        ctx.fillStyle = rgbToHex(r,g,b);
+        ctx.fillRect(colunm,row,1,1);
+        colunm++
+    }
+}
+
+function calculaMediana(numbers){
+    numbers.sort(function(a, b){return a-b})
+    return numbers[4]
+}
+
+function laplaciano(mult, div){
+    const {width, height} = inImg
+    const size = width*height
+    const src = new Uint32Array(inImg.data.buffer)
+
+    const ctx = canvasProcessado.getContext('2d')
+    canvasProcessado.width = inImg.width
+    canvasProcessado.height = inImg.height
+
+    let row = 0
+    let colunm = 0 
+    
+    for (let i = 0; i < size; i++) {
+        let r,g,b
+        if(!colunm || colunm == width-1 || !row || row == height-1){
+            r = src[i] & 0xFF
+            g = (src[i] >> 8) & 0xFF
+            b = (src[i] >> 16) & 0xFF
+        }else{
+            r = Math.abs(parseInt((( src[i-width-1] & 0xFF)*(-1)+        (src[i-width] & 0xFF)*(-1)+         (src[i-width+1] & 0xFF)*(-1)+           (src[i-1] & 0xFF)*(-1)+         (src[i] & 0xFF)*(mult)+            (src[i+1] & 0xFF)*(-1)+         (src[i+width-1] & 0xFF)*(-1)+           (src[i+width] & 0xFF)*(-1)+         (src[i+width+1] & 0xFF)*(-1))/div))
+            g = Math.abs(parseInt((((src[i-width-1] >> 8) & 0xFF)*(-1)+  ((src[i-width] >> 8) & 0xFF)*(-1)+ ((src[i-width+1] >> 8) & 0xFF)*(-1)+     ((src[i-1] >> 8) & 0xFF)*(-1)+  ((src[i] >> 8) & 0xFF)*(mult)+     ((src[i+1] >> 8) & 0xFF)*(-1)+  ((src[i+width-1] >> 8) & 0xFF)*(-1)+    ((src[i+width] >> 8) & 0xFF)*(-1)+  ((src[i+width+1] >> 8) & 0xFF)*(-1))/div))
+            b = Math.abs(parseInt((((src[i-width-1] >> 16) & 0xFF)*(-1)+ ((src[i-width] >> 16) & 0xFF)*(-1)+ ((src[i-width+1] >> 16) & 0xFF)*(-1)+   ((src[i-1] >> 16) & 0xFF)*(-1)+ ((src[i] >> 16) & 0xFF)*(mult)+    ((src[i+1] >> 16) & 0xFF)*(-1)+ ((src[i+width-1] >> 16) & 0xFF)*(-1)+   ((src[i+width] >> 16) & 0xFF)*(-1)+ ((src[i+width+1] >> 16) & 0xFF)*(-1))/div))
+        }
+
+        if(i && !(i % width)){
+            row++
+            colunm = 0
+        }
+
+        ctx.fillStyle = rgbToHex(r,g,b);
+        ctx.fillRect(colunm,row,1,1);
+        colunm++
+    }
 }
